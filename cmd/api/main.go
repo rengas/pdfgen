7ab6b1865b43909e72244448962499ac2b6b60cd
rengas <mr.rengaswamy@gmail.com>
2022-10-28 23:53:38 +0800

    remove firebase

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"flag"
@@ -11,8 +10,6 @@ import (
 	"github.com/rengas/pdfgen/pkg/account"
 	"github.com/rengas/pdfgen/pkg/dbutils"
 	"github.com/rengas/pdfgen/pkg/design"
-	"github.com/rengas/pdfgen/pkg/firebase"
-	"github.com/rengas/pdfgen/pkg/middleware"
 	"github.com/rengas/pdfgen/pkg/minifier"
 	"github.com/rengas/pdfgen/pkg/pdfrender"
 	"github.com/rengas/pdfgen/pkg/server"
@@ -27,8 +24,7 @@ import (
 var (
 	addr            = flag.String("addr", ":8080", "Application http server network address")
 	shutdownTimeout = flag.Duration("shutdown-timeout", 30*time.Second, "Graceful shutdown timeout")
-	connString      = flag.String("pg-conn-string", "postgres://pdfgen:pdfgen@localhost:5432/pdfgen?sslmode=disable", "PostgresSQL server connection string")
-	firebasePath    = flag.String("firebase-path", "./firebase.json", "firebase creds")
+	connString      = flag.String("pg-conn-string", "postgres://pdfgen:pdfgen@pg:5432/pdfgen?sslmode=disable", "PostgresSQL server connection string")
 	renderDBPath    = flag.String("render-db-path", "/etc/secrets/staging.json", "staging db creds")
 )
 
@@ -71,10 +67,9 @@ func main() {
 		}
 		v, ok := secrets["pg-conn-string"]
 		if !ok {
-			log.Fatal("unable to get pg-conn-string from environmet")
+			log.Fatal("unable to get pg-conn-string from environment")
 		}
 		*connString = v
-		*firebasePath = "/etc/secrets/firebase.json"
 	}
 
 	db := dbutils.MustOpenPostgres(*connString)
@@ -87,18 +82,11 @@ func main() {
 	designAPI := NewDesignAPI(designRepo, minify)
 	generatorAPI := NewGeneratorAPI(designRepo, renderer)
 
-	b, err := os.ReadFile(*firebasePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fAuth := firebase.New(bytes.NewBuffer(b))
-
 	log.Println("initialising routes...")
 	r := chi.NewRouter()
 	r.Use(m.RequestID)
 
 	r.Group(func(r chi.Router) {
-		r.Use(middleware.NewFirebaseAuth(fAuth, profileRepo).FirebaseAuth)
 		r.Use(m.Logger)
 
 		r.Route("/design", func(r chi.Router) {
@@ -120,7 +108,6 @@ func main() {
 	})
 
 	r.Group(func(r chi.Router) {
-		r.Use(middleware.NewFirebaseAuth(fAuth, profileRepo).FirebaseAuthWithout)
 		r.Post("/profile", profileAPI.CreateProfile)
 	})
 	r.Group(func(r chi.Router) {
@@ -129,26 +116,6 @@ func main() {
 
 	log.Println("starting api...")
 	s := server.NewHTTPServer(*addr, r, *shutdownTimeout)
-
-	//TODO need to find a way to put this back and remove migrate from docker-compose
-	/*if os.Getenv("env") == "staging" {
-		driver, err := postgres.WithInstance(db, &postgres.Config{})
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		sourceUrl := fmt.Sprintf("file://%s", "migrations")
-		m, err := migrate.NewWithDatabaseInstance(
-			sourceUrl,
-			"postgres", driver)
-		if err != nil {
-			log.Fatal(err)
-		}
-		err = m.Up()
-		if err != nil && !errors.Is(err, migrate.ErrNoChange) {
-			log.Fatal(err)
-		}
-	}*/
 
 	s.Start()
 
